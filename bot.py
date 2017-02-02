69db1ec0908852b43a51aeb6de1d3f606c5ba3d1
lyserg <lyserg.z@gmail.com>
2017-02-02 02:53:19 -0300

    More experiments
    About to try this Config thing, so might as well commit now in case I can't make it work like I want to

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import discord
 import asyncio
+from discord.ext import commands
+import traceback
+import sys
 import secrets
 import hinter
 from discord.ext.commands import Bot
@@ -12,15 +15,70 @@ NFTestBot = Bot(command_prefix="!")
 async def on_ready():
     print("DFTB-DFTB-DFTB READY!")
 
-# Anotate your command methods this way, using "command()" after your bot's name
-@NFTestBot.command()
-async def hello(*args):
-    return await NFTestBot.say("YO!!")
 
+# Anotate your command methods this way, using "command()" after your bot's name
+@NFTestBot.command(pass_context=True)
+async def hello(context, member: discord.Member = None):
+    # "context" is the context that's being passed.
+    # I don't know why you need to make member default to None, but you do.
+    if member is None:
+        member = context.message.author
+    botname = NFTestBot.user.name
+    return await NFTestBot.say("YO " + member.mention + "!!")
 
 @NFTestBot.command(pass_context=True)
 async def waluigi(ctx):
-    await NFTestBot.say("Hey, " + ctx.message.author + "http://i.imgur.com/GfR30jl.gif")
+    # Just a test. This method will PM whoever used this command
+    return await NFTestBot.send_message(ctx.message.author,
+                                 "Hey, " + ctx.message.author.name + " http://i.imgur.com/GfR30jl.gif")
+
+# This method triggers when the event of a member joining occurs.
+@NFTestBot.event
+async def on_member_join(member):
+    # Greets whoever joined in (I'm assuming) the default channel of 'server' (usually #general).
+    server = member.server
+    # Finds a channel so that we can link to it in our welcome message
+    useful_channel = discord.utils.get(server.channels, name='rules_and_information')
+    welcome = "Hey, {0.mention}! Welcome to {1.name}!" \
+              "I am {2.user.name} an automated bot. You can see a list of my commands by messaging me '!help'" \
+              "(just click on my name from the members list and send me that message).\n"\
+              "First, you should go to {3.mention} for general directions (just click that name!)" \
+              "Other than that, the green peeps over there in the members list are the admin team, " \
+              "they, and really everyone else, can help you with anything you need. Have fun and DFTBA!"
+    return await NFTestBot.send_message(server, welcome.format(member, server, NFTestBot, useful_channel))
+
+
+# Shuts the bot down. Admins only
+@NFTestBot.command(pass_context=True)
+async def kill_bot(ctx, member: discord.Member = None):
+    if member is None:
+        member = ctx.message.author
+    if not member.server_permissions.administrator:
+        await NFTestBot.say("Lol. You need to evolve into an admin if you want to take me on pal Q('-'Q).")
+    await NFTestBot.say("That's not even nice, but ok...")
+    return await NFTestBot.logout()
+
+@NFTestBot.command(pass_context=True, aliases=["memberCount", "countMembers", "count_members"])
+async def member_count(ctx):
+    server = ctx.message.server
+    count = server.member_count
+    return await NFTestBot.say("There are {} members in this server.".format(count))
+
+
+
+@NFTestBot.event
+async def on_command_error(error, ctx):
+    if isinstance(error, commands.CommandNotFound):
+        return await NFTestBot.send_message(ctx.message.channel, 'The command you attempted to use does not exist')
+    elif isinstance(error, commands.CommandInvokeError):
+        print('In {0.command.qualified_name}:'.format(ctx), file=sys.stderr)
+        traceback.print_tb(error.original.__traceback__)
+    elif isinstance(error, commands.BadArgument):
+        return await NFTestBot.send_message(ctx.message.channel, 'Whoa, something was wrong with '
+                                                                 'the arguments you passed there')
+    print('{0.__class__.__name__}: {0}'.format(error.original), file=sys.stderr)
+    return
+
 
 # You can add commands from another class this way:
 NFTestBot.add_cog(hinter.Hinter(NFTestBot))
--- hinter.py
+++ hinter.py
@@ -1,10 +1,10 @@
 from discord.ext import commands
-
+import config
 
 class Hinter:
     def __init__(self, bot):
         # When you see how hints is used you will think "OMFG WHAT ABOUT THE RACE CONDITIONS"... you will be right.
-        self.hints = {}
+        self.config = config.Config('hints.json')
         self.bot = bot
 
     # Use "commands" to later add this commands to a bot (in this case self.bot) as part of a cog. See bot.py for more.
@@ -14,35 +14,42 @@ class Hinter:
                                                                "the hint uses more than one word.")
     async def add_hint(self, hintname, hint):
         if hintname in self.hints:
-            await self.bot.say("A hint named '" + hintname + "' already exists.\n"
+            return await self.bot.say("A hint named '" + hintname + "' already exists.\n"
                           "please delete existing hint using '!del_hint' or choose another name.")
         else:
             # You may be thinking "OMFG RACECONDITIONS"... you are right.
             self.hints[hintname] = hint
-            await self.bot.say("Added hint '" + hintname + "'.")
+            return await self.bot.say("Added hint '" + hintname + "'.")
 
     @commands.command(aliases=["delHint", "DelHint"], description="Give me a topic or hint name and I will give you"
                                                                   "a hint for it. If the name is more than one word"
                                                                   "long, surround it with \"quotation marks\"")
     async def del_hint(self, hintname):
         if hintname not in self.hints:
-            await self.bot.say("There is no hint named '" + hintname + "'.")
+            return await self.bot.say("There is no hint named '" + hintname + "'.")
         else:
             del self.hints[hintname]
-            await self.bot.say("Deleted hint '" + hintname + "'.")
+            return await self.bot.say("Deleted hint '" + hintname + "'.")
 
-    @commands.command()
+    @commands.command(aliases=["hintPls"])
     async def hint_pls(self, hintname):
         if hintname not in self.hints:
-            await self.bot.say("Sorry, there's no hint for '" + hintname + "'.")
+            return await self.bot.say("Sorry, there's no hint for '" + hintname + "'.")
         else:
             hint = self.hints[hintname]
-            await self.bot.say(hint)
+            return await self.bot.say(hint)
 
     @commands.command()
     async def hints_list(self):
         #Backticks are used for code formatting which gives the list a nice look
+        if not self.hints:
+            return await self.bot.say("Sorry, there are no hints at all... but ask humans, I hear they are nice")
         result = "We have hints for the following topics:\n```"
         for hintname in self.hints:
             result += hintname + "\n"
         result += "```"
+        return await self.bot.say(result)
+
+    @commands.command()
+    async def save_hints(self):
+        return
