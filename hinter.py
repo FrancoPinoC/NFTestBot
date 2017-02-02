7b654917e5258f43d5d2b7f9e5b7f07993b3b017
lyserg <lyserg.z@gmail.com>
2017-02-02 05:24:20 -0300

    Hinter now uses Config \o/
    Also fixed/changed some other stuff but I forget what

--- hinter.py
+++ hinter.py
@@ -1,55 +1,63 @@
+# Use of Config was copied from here:
+# https://github.com/Rapptz/RoboDanny/blob/master/cogs/pokemon.py
 from discord.ext import commands
 import config
 
+
 class Hinter:
     def __init__(self, bot):
-        # When you see how hints is used you will think "OMFG WHAT ABOUT THE RACE CONDITIONS"... you will be right.
         self.config = config.Config('hints.json')
         self.bot = bot
 
     # Use "commands" to later add this commands to a bot (in this case self.bot) as part of a cog. See bot.py for more.
-    @commands.command(aliases=["addHint", "AddHint"], description="Add a hint. First argument is the name you are "
-                                                               "going to use to later retrieve the hint, the second "
-                                                               "argument is the hint itself. Surround with quotes if "
-                                                               "the hint uses more than one word.")
-    async def add_hint(self, hintname, hint):
-        if hintname in self.hints:
-            return await self.bot.say("A hint named '" + hintname + "' already exists.\n"
-                          "please delete existing hint using '!del_hint' or choose another name.")
+    @commands.command(aliases=["addHint", "AddHint"],
+                      description="Add a hint. First argument is the name or topic you are going to use to later "
+                                  "retrieve the hint, the second argument is the hint itself. Surround with quotes if "
+                                  "the hint uses more than one word.")
+    async def add_hint(self, hint_name, hint_content):
+        hints = self.config.all()
+        try:
+            hint = hints[hint_name]
+        except KeyError:
+            # not found, so create it. Relying on errors is bad form... but eh.
+            await self.config.put(hint_name, hint_content)
+            return await self.bot.say("A hint for '" + hint_name + "' has been added!")
         else:
-            # You may be thinking "OMFG RACECONDITIONS"... you are right.
-            self.hints[hintname] = hint
-            return await self.bot.say("Added hint '" + hintname + "'.")
+            # Get hint:
+            return await self.bot.say("There's already a hint for {0}! It's this:\n"
+                                      "```\n{1}\n```".format(hint_name, hint))
 
     @commands.command(aliases=["delHint", "DelHint"], description="Give me a topic or hint name and I will give you"
                                                                   "a hint for it. If the name is more than one word"
                                                                   "long, surround it with \"quotation marks\"")
-    async def del_hint(self, hintname):
-        if hintname not in self.hints:
-            return await self.bot.say("There is no hint named '" + hintname + "'.")
+    async def del_hint(self, hint_name):
+        try:
+            await self.config.remove(hint_name)
+        except KeyError:
+            return await self.bot.say("There wasn't any hint for '" + hint_name +
+                                      "', please add a hint for that so you can delete it :robot:")
         else:
-            del self.hints[hintname]
-            return await self.bot.say("Deleted hint '" + hintname + "'.")
+            return await self.bot.say(hint_name + " has been removed from the hints list.")
 
     @commands.command(aliases=["hintPls"])
-    async def hint_pls(self, hintname):
-        if hintname not in self.hints:
-            return await self.bot.say("Sorry, there's no hint for '" + hintname + "'.")
+    async def hint_pls(self, hint_name):
+        hints = self.config.all()
+        try:
+            hint = hints[hint_name]
+        except KeyError:
+            return await self.bot.say("There's not hint for " + hint_name + ". Maybe ask someone else :[?")
         else:
-            hint = self.hints[hintname]
-            return await self.bot.say(hint)
+            return await self.bot.say('The hint for %s is:\n ```\n%s\n```' % (hint_name, hint))
 
-    @commands.command()
+    @commands.command(aliases=["hintsList", "hint_list", "hintList", "all_hints", "allHints"])
     async def hints_list(self):
         #Backticks are used for code formatting which gives the list a nice look
-        if not self.hints:
+        hints = self.config.all()
+        if not hints:
             return await self.bot.say("Sorry, there are no hints at all... but ask humans, I hear they are nice")
-        result = "We have hints for the following topics:\n```"
-        for hintname in self.hints:
-            result += hintname + "\n"
+        result = "We have hints for the following topics:\n```\n"
+        print(hints)
+        for hint_name in hints:
+            result += hint_name + "\n"
         result += "```"
         return await self.bot.say(result)
-
-    @commands.command()
-    async def save_hints(self):
-        return
